bcca17c845bac705c28dbb1000ea42dff11e713a
TrungHoang <ha2kv3@gmail.com>
2024-01-25 07:35:11 +0700

    Update Vnpay

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ db.connect()
 app.use(morgan('dev'))
 app.use(cors())
 // app.use(cors({
-//   origin: 'http://localhost:5173',
+//   origin: 'http://localhost:3000',
 //   credentials: true,
 //   optionsSuccessStatus: 200
 // }));
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const { getAllUsers, getUserById, updateUserById, deleteUserById, createUserByAdmin } = require('../controllers/user.controllers')
-const { getAllInstructorByAdmin } = require('../controllers/instructor.controllers')
+const { getAllInstructorByAdmin, getInstructorByAdmin, updateInstructorByAdmin, deleteInstructorByAdmin } = require('../controllers/instructor.controllers')
+const { getCoursesByAdmin, getCourseByAdmin, updateCourseByAdmin, deleteCourseByAdmin } = require('../controllers/course.controllers')
 
 router.get('/users', getAllUsers)
 router.get('/users/:id', getUserById)
@@ -10,5 +11,13 @@ router.delete('/users/:id', deleteUserById)
 router.post('/users', createUserByAdmin)
 
 router.get('/instructors', getAllInstructorByAdmin)
+router.get('/instructors/:id', getInstructorByAdmin)
+router.put('/instructors/:id', updateInstructorByAdmin)
+router.delete('/instructors/:id', deleteInstructorByAdmin)
+
+router.get('/courses', getCoursesByAdmin )
+router.get('/courses/:id', getCourseByAdmin)
+router.put('/courses/:id', updateCourseByAdmin)
+router.delete('/courses/:id', deleteCourseByAdmin)
 
 module.exports = router
